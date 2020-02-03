6fa1691f18a778dec68d34d1d00a3788da677058
Valmik Vyas <valmik.vyas1@gmail.com>
2020-02-03 07:49:42 -0500

    second

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+const USERS_URL = `https://acme-users-api-rev.herokuapp.com/api/users/random`;
 const cards = document.querySelector("#cards");
 
-let users = []
+const renderPage = user => {
+  const pager = document.querySelector("#pager");
+  const _idx = window.location.hash.slice(1) || 0;
+  const user1 = user[0];
+  const user2 = user[1];
+  const user3 = user[2];
 
+  window.addEventListener("hashchange", () => {
+    const hash = window.location.hash.slice(1);
+    
+  });
 
-const USER1_URL = `https://acme-users-api-rev.herokuapp.com/api/users/random`;
+  const pages = new Array(user.length).fill("").map((_, idx) => {
+    return {
+      selected: _idx == idx,
+      text: idx + 1,
+      idx
+    };
+  });
 
+  const html = pages
+    .map(page => {
+      return `
+    <li ${page.selected ? "class = selected" : ""}>
+    <a href = '#${page.idx}'>${page.text}</a>
+    </li>
+    `;
+    })
+    .join("");
 
+  pager.innerHTML = html;
+  // console.log(pager);
+};
 
 const renderUsers = user => {
-
-  
   const html = user
     .map(user => {
       return `
       <div>
-        <h2>${user.fullName}</h2>
+        <p>${user.fullName}</p>
         <p> ${user.email}</p>
-        <img src="${user.avatar}">
+        <img id = 'image' src="${user.avatar}">
         </div>
-        
+      
         `;
     })
     .join("");
@@ -26,12 +52,12 @@ const renderUsers = user => {
   cards.innerHTML = html;
 };
 
-Promise.all([fetch(USER1_URL), fetch(USER1_URL), fetch(USER1_URL)])
+Promise.all([fetch(USERS_URL), fetch(USERS_URL), fetch(USERS_URL)])
   .then(response => Promise.all(response.map(r => r.json())))
   .then(data => {
-    users = data
     console.log(data[0]);
     console.log(data[1]);
     console.log(data[2]);
     renderUsers(data);
+    renderPage(data);
   });

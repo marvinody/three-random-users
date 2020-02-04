7e828097c3864a653f543ec057ede4956d460969
RobertPeng <jenpopeng@gmail.com>
2020-02-04 09:38:14 -0500

    submit final

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const render3RandomUsers = (userObjsArr) => {
         const num = userObjsArr.indexOf(user) + 1;
         return `
         <div class='userbox'>
-            <h3 class='numberBox' id='number${num}'><a href='#${num}'>${num}</a></h3>
+            <a class='numberBox' id='number${num}' href='#${num}'>${num}</a>
+            
             <ul class='userInfo' id='userInfo${num}'>
             <li>${user.fullName}</li>
             <li>${user.email}</li>
@@ -21,15 +22,7 @@ const render3RandomUsers = (userObjsArr) => {
 }
 
 const p = Promise.all([fetch(randomUserURL),fetch(randomUserURL),fetch(randomUserURL)])
-    // .then(response => console.log(response))
     .then(response => Promise.all(response.map(r=>r.json())))
-    // .then(data => console.log(data[0]))
-    // .then(data => {
-    //     let user1 = data[0];
-    //     let user2 = data[1];
-    //     let user3 = data[2];
-    //     console.log(user1);
-    // })
     .then(data => render3RandomUsers(data))
 
 const notSelected = (numArr, selectedNum) => {
@@ -41,29 +34,25 @@ const notSelected = (numArr, selectedNum) => {
    
 window.addEventListener('hashchange', () => {
     let selectedId = window.location.hash.slice(1);
-    //console.log(typeof selectedId);
-    //console.log(selectedId);
     selectedId = parseInt(selectedId);
-    
     let unSelectedArr = notSelected([1,2,3], selectedId); 
-    //console.log(unSelectedArr);
+
+    const allNumberboxs = document.querySelectorAll(`a`);
 
     if (unSelectedArr.length === 0) {
         const allItems = document.querySelectorAll(`ul`);
-        //console.log(allItems);
-        allItems.forEach(item => item.style.visibility = 'visible')    
+        allItems.forEach(item => item.style.visibility = 'visible'); 
+        allNumberboxs.forEach(item => item.classList.remove('selected'));   
     } else {
+        allNumberboxs.forEach(item => item.classList.remove('selected'));
         const selectedItem = document.querySelector(`ul#userInfo${selectedId}`);
         selectedItem.style.visibility = 'visible'
+        const selectedNumberBox = document.querySelector(`a#number${selectedId}`);
+        selectedNumberBox.classList.add('selected');
         const unselectedUserInfo1 = document.querySelector(`ul#userInfo${unSelectedArr[0]}`);
         const unselectedUserInfo2 = document.querySelector(`ul#userInfo${unSelectedArr[1]}`);
-        //console.log(unselectedUserInfo1,unselectedUserInfo2);
         unselectedUserInfo1.style.visibility = 'hidden';
         unselectedUserInfo2.style.visibility = 'hidden';
     }
-    
-    //const selectedNumberBox = document.querySelector(`h3#number${selectedId}`);
-    //console.log(selectedNumberBox);
-    //selectedNumberBox.classList.toggle('selected');
 })
 

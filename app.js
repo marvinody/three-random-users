57cd5542edb698aa264d7be337517fbf0201566b
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-02-04 03:20:31 -0500

    almostFinished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let nameCard = document.querySelector(".nameCard");
 let users = document.querySelector(".users");
 let list;
 let dataThreeUsers = [];
-const getUsers = function() {
+const getUsers = async function() {
   const user1 = fetch(
     "https://acme-users-api-rev.herokuapp.com/api/users/random"
   );
@@ -15,38 +15,62 @@ const getUsers = function() {
   );
   const threeUsers = Promise.all([user1, user2, user3]);
 
-  return threeUsers
-    .then(response => {
-      const user1Data = response[0];
-      const user2Data = response[1];
-      const user3Data = response[2];
-      const threeUserData = [
-        user1Data.json(),
-        user2Data.json(),
-        user3Data.json()
-      ];
-      return Promise.all(threeUserData);
-    })
-    .then(data => {
-      const dataUser1 = data[0];
-      const dataUser2 = data[1];
-      const dataUser3 = data[2];
-      dataThreeUsers = [dataUser1, dataUser2, dataUser3];
-      console.log(data);
-      //return dataThreeUsers;
-    });
+  const response = await threeUsers;
+  const user1Data = response[0];
+  const user2Data = response[1];
+  const user3Data = response[2];
+  const threeUserData = [user1Data.json(), user2Data.json(), user3Data.json()];
+  const data = await Promise.all(threeUserData);
+  const dataUser1 = data[0];
+  const dataUser2 = data[1];
+  const dataUser3 = data[2];
+  dataThreeUsers = [dataUser1, dataUser2, dataUser3];
 };
 getUsers();
-console.log(dataThreeUsers);
+function reload() {
+  location.reload();
+}
 
-links.addEventListener("click", function(ev) {
-  ev.preventDefault();
-  console.log(ev.target.innerHTML);
-  document.querySelector(`.user${ev.target.innerHTML}`).innerHTML =
-    dataThreeUsers[ev.target.innerHTML - 1].fullName +
-    dataThreeUsers[ev.target.innerHTML - 1].email +
-    `<img class='image' src=${dataThreeUsers[ev.target.innerHTML - 1].avatar}>`;
+function removeUsers() {
+  let index = location.hash.slice(1);
+  index = parseInt(index);
+  for (let i = 0; i < 3; ) {
+    document.querySelector(`.user${index + 1}`).innerHTML = "";
+
+    document.querySelector(`.user${index + 1}`).classList.remove("card");
+  }
+}
+// links.addEventListener("click", function() {
+//   removeUsers();
+// });
+window.addEventListener("hashchange", function(ev) {
+  //removeUsers();
+  getUsers();
+  let index = location.hash.slice(1);
+  index = parseInt(index);
+  console.log(typeof index);
+  if (index === 3) {
+    for (let i = 0; i < 3; i++) {
+      console.log(i);
+      let userSelector = document.querySelector(`.user${i + 1}`);
+      document.querySelector(`.user${i + 1}`).innerHTML =
+        dataThreeUsers[i].fullName +
+        "<br>" +
+        dataThreeUsers[i].email +
+        "<br>" +
+        `<img style='margin-top:25px' class='image' src=${dataThreeUsers[i].avatar}>`;
+      userSelector.classList.add("card");
+    }
+  }
+  document.querySelector(`.user${index + 1}`).innerHTML =
+    dataThreeUsers[index].fullName +
+    "<br>" +
+    dataThreeUsers[index].email +
+    "<br>" +
+    `<img  style='margin-top:25px' class='image' src=${dataThreeUsers[index].avatar}>`;
+  document.querySelector(`.user${index + 1}`).classList.add("card");
 });
+
 // getUsers();
 // getUsers().then(dataThreeUsers => {
 //   console.log(dataThreeUsers);

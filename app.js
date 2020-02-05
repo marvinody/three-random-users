c876c13ea5df64951de5318a78cb478b6d1b2a7a
Manuel Bugallo <marvinody@gmail.com>
2020-02-04 20:28:38 -0500

    added comments

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ const user2Promise = fetch(randomUrl)
 const user3Promise = fetch(randomUrl)
     .then(response => response.json())
 
+// good promise.all
 Promise.all([user1Promise, user2Promise, user3Promise])
     .then(result => {
-        render(result)
+        render(result) // and then nice use of render!
     })
 
 function render(arr) {
-
+    // if you're using a map, I expect you to assign the result of it.
+    // maybe use a forEach if you're doing side effect stuff...
     arr.map((user, idx) => {
         const usersList = document.querySelector(`#user${idx + 1}`)
 
+        // good idea using a ternary!! with some data on obj. I like it
         const userHtml = `
             <div ${ user.selected ? "class='selected'" : ''}>
                 <a href='#${idx + 1}'>
@@ -31,7 +34,12 @@ function render(arr) {
                 <li><img id="profpic" src="${user.avatar}"></li>
             </ul>
         `
-
+        // if you try changing person like 7-9 times, your computer might slow down
+        // that's because each rerender is adding a NEW event listener.
+        // we only want to add event listeners ONCE (generally, like 90% of time)
+        // so move this thing outside and only do it once
+        // you may have to change your logic but doing it this way, you end up with 3 listeners EACH click
+        // no bueno
         window.addEventListener('hashchange', () => {
             const page = window.location.hash.slice(1)
 

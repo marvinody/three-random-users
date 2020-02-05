4dbe84e4d51f7afb4d040f5d273efd15f2d468da
Manuel Bugallo <marvinody@gmail.com>
2020-02-05 12:08:05 -0500

    linting

--- build.js
+++ build.js
@@ -1,46 +1,45 @@
-let random = "https://acme-users-api-rev.herokuapp.com/api/users/random";
-const rootId = document.getElementsByTagName("MAIN")[0];
-const h1 = document.getElementsByTagName("H1")[0];
-console.log(h1);
+let random = 'https://acme-users-api-rev.herokuapp.com/api/users/random'
+const rootId = document.getElementsByTagName('MAIN')[0]
+const h1 = document.getElementsByTagName('H1')[0]
+console.log(h1)
 const getData = (api) => {
 	Promise.all([fetch(api), fetch(api), fetch(api)]).then((response) =>
 		Promise.all(response.map((result) => result.json())).then((data) => {
-			renderHTML(data);
-		})
-	);
-};
+			renderHTML(data)
+		}))
+}
 
 const renderHTML = (apiCalls) => {
 	apiCalls
 		.map((user, idx) => {
-			idx++;
-			return `<div id="card-holder"><a href="#${user.id}">${idx}</a><div><p>${user.fullName}</p> <p>${user.email}</p> <img src="${user.avatar}" alt="${user.fullName}"/></div></div>`;
+			idx++
+			return `<div id="card-holder"><a href="#${user.id}">${idx}</a><div><p>${user.fullName}</p> <p>${user.email}</p> <img src="${user.avatar}" alt="${user.fullName}"/></div></div>`
 		})
 		.forEach((user) => {
-			rootId.innerHTML += user;
-		});
-};
+			rootId.innerHTML += user
+		})
+}
 
-window.addEventListener("hashchange", (event) => {
-	const id = window.location.hash.slice(1);
-	const arrId = [...document.querySelectorAll("a[href]")];
+window.addEventListener('hashchange', (event) => {
+	const id = window.location.hash.slice(1)
+	const arrId = [...document.querySelectorAll('a[href]')]
 	arrId.forEach((a) => {
-		let div = a.nextSibling;
+		let div = a.nextSibling
 		if (a.hash.slice(1).includes(id)) {
-			a.classList.add("border-link");
-			div.classList.remove("hide");
+			a.classList.add('border-link')
+			div.classList.remove('hide')
 		} else {
-			a.classList.remove("border-link");
-			div.classList.add("hide");
+			a.classList.remove('border-link')
+			div.classList.add('hide')
 		}
-	});
-});
+	})
+})
 
-h1.addEventListener("click", (event) => {
-	const arrId = [...document.querySelectorAll("a[href]")];
+h1.addEventListener('click', (event) => {
+	const arrId = [...document.querySelectorAll('a[href]')]
 	arrId.forEach((a) => {
-		a.classList.remove("border-link");
-		a.nextSibling.classList.remove("hide");
-	});
-});
-getData(random);
+		a.classList.remove('border-link')
+		a.nextSibling.classList.remove('hide')
+	})
+})
+getData(random)

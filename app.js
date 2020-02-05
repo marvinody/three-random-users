70b92aa3ce7cc6a7bab61e471fb80778ab00f284
Manuel Bugallo <marvinody@gmail.com>
2020-02-04 19:52:25 -0500

    added comments

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 
-
+// good naming this outside incase it were to change!
 const API = 'https://acme-users-api-rev.herokuapp.com/api/users/random'
 const usersList = document.querySelector('#usersList')
 
 
+// maybe we can go over hashchange later at some point?
 window.addEventListener('hashchange', (ev) => {
+  // you were able to grab the id!
+  // maybe next time you can leave some pseudocode as to what should happen?
   const id = window.location.hash.slice(1)
   console.log(id)
 })
 
+// very clean with promise.all!
 function fetchUserData() {
   const user1 = fetch(API)
   const user2 = fetch(API)
@@ -23,6 +27,8 @@ function fetchUserData() {
 
       return Promise.all([user1info.json(), user2info.json(), user3info.json()])
     })
+    // this seems weird. You split them out only to join them back?
+    // Make sure you know what each line you write is doing!
     .then(JSONs => {
       const user1JSON = JSONs[0]
       const user2JSON = JSONs[1]

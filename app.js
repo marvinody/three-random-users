f30836a04a8edbe9a98562ca261de297ead4d787
Manuel Bugallo <marvinody@gmail.com>
2020-02-05 11:13:54 -0500

    linting

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const main = document.querySelector("main");
-const title = document.querySelector("h1");
+const main = document.querySelector('main')
+const title = document.querySelector('h1')
 
 const urls = [
-  "https://acme-users-api-rev.herokuapp.com/api/users/random",
-  "https://acme-users-api-rev.herokuapp.com/api/users/random",
-  "https://acme-users-api-rev.herokuapp.com/api/users/random"
-];
+  'https://acme-users-api-rev.herokuapp.com/api/users/random',
+  'https://acme-users-api-rev.herokuapp.com/api/users/random',
+  'https://acme-users-api-rev.herokuapp.com/api/users/random'
+]
 
 const fetchAll = Promise.all(
   urls.map(url => fetch(url).then(response => response.json()))
-);
+)
 
 const renderUsers = () => {
   fetchAll.then(users => {
-    let i = 0;
+    let i = 0
     const html = users
       .map(user => {
-        i++;
-        user.number = i;
+        i++
+        user.number = i
         return `<div class="userContainer">
                 <a class="number" href="#${i}">${i}</a>
                 <div class="user${i}">
@@ -27,28 +27,28 @@ const renderUsers = () => {
                     <img class="avatar" src="${user.avatar}" alt="">
                   </div>
                 </div>
-              </div>`;
+              </div>`
       })
-      .join("");
+      .join('')
 
-    main.innerHTML = html;
-    return users;
-  });
-};
-renderUsers();
+    main.innerHTML = html
+    return users
+  })
+}
+renderUsers()
 
-window.addEventListener("hashchange", ev => {
-  ev.preventDefault();
-  const userNumber = window.location.hash.slice(1);
+window.addEventListener('hashchange', ev => {
+  ev.preventDefault()
+  const userNumber = window.location.hash.slice(1)
   fetchAll.then(users => {
-    console.log(users);
+    console.log(users)
     const clickedUser = users.find(user => {
-      console.log(userNumber);
-      console.log(user.number);
-      return parseInt(userNumber) === user.number;
-    });
-    console.log(clickedUser);
-    const i = clickedUser.number;
+      console.log(userNumber)
+      console.log(user.number)
+      return parseInt(userNumber) === user.number
+    })
+    console.log(clickedUser)
+    const i = clickedUser.number
     const html = `<div class="userContainer">
     <a class="number" href="#${i}">${i}</a>
     <div class="user${i}">
@@ -58,12 +58,12 @@ window.addEventListener("hashchange", ev => {
         <img class="avatar" src="${clickedUser.avatar}" alt="">
       </div>
     </div>
-  </div>`;
-    main.innerHTML = html;
-  });
-});
+  </div>`
+    main.innerHTML = html
+  })
+})
 
-title.addEventListener("click", ev => {
-  window.location.hash = "";
-  renderUsers();
-});
+title.addEventListener('click', ev => {
+  window.location.hash = ''
+  renderUsers()
+})

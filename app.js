399fc71b7cd5ed57e85071bef02d657ec954ca80
Manuel Bugallo <marvinody@gmail.com>
2020-02-05 11:49:14 -0500

    added comments

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const main = document.querySelector('main')
 const title = document.querySelector('h1')
-
+// good use of const throughout!
 const urls = [
   'https://acme-users-api-rev.herokuapp.com/api/users/random',
   'https://acme-users-api-rev.herokuapp.com/api/users/random',
   'https://acme-users-api-rev.herokuapp.com/api/users/random'
 ]
 
+// beautiful use of map! exactly as should be done!!
 const fetchAll = Promise.all(
   urls.map(url => fetch(url).then(response => response.json()))
 )
@@ -15,6 +16,8 @@ const renderUsers = () => {
   fetchAll.then(users => {
     let i = 0
     const html = users
+      // take a look at the .map method to see what gets passed into this callback
+      // you get the value (user), but you also get some other goodies...
       .map(user => {
         i++
         user.number = i
@@ -32,19 +35,24 @@ const renderUsers = () => {
       .join('')
 
     main.innerHTML = html
+    // returning users is a good idea here generally
+    // but I'm unsure if you need it. I think it's good to do this though
+    // will prevent bugs in the future if you can keep it up!
     return users
   })
 }
 renderUsers()
 
 window.addEventListener('hashchange', ev => {
-  ev.preventDefault()
+  ev.preventDefault() // unsure if needed but not hurtful to have
   const userNumber = window.location.hash.slice(1)
   fetchAll.then(users => {
-    console.log(users)
+    console.log(users) // console.log should be removed
     const clickedUser = users.find(user => {
-      console.log(userNumber)
-      console.log(user.number)
+      console.log(userNumber) // console.log should be removed
+      console.log(user.number) // console.log should be removed
+      // if you use parseInt, you should pass in a 2nd parameter
+      // or just use Number and it'll work a little more expected
       return parseInt(userNumber) === user.number
     })
     console.log(clickedUser)
@@ -63,6 +71,9 @@ window.addEventListener('hashchange', ev => {
   })
 })
 
+// hmmmmm
+// not the way we were expecting but it's alright. I would like to see your code
+// in hash change handle having to show everyone
 title.addEventListener('click', ev => {
   window.location.hash = ''
   renderUsers()

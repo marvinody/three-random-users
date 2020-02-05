a5b97a2717d9838949c71b0662d6e07b03eec152
Manuel Bugallo <marvinody@gmail.com>
2020-02-04 21:53:00 -0500

    linting

--- app.js
+++ app.js
@@ -8,38 +8,37 @@ const user3 = fetch(API).then(result => result.json())
 
 window.location.hash = ''
 
-function render (user, userCount){
+function render(user, userCount) {
     let card = document.querySelector(`#user${userCount}`)
     let check = document.querySelector(`#user${userCount}>.card`)
-    
-    if(check == null){
-    
-    let html = `<div class='card'> 
+
+    if (check == null) {
+
+        let html = `<div class='card'>
         <div>${user.fullName}</divs>
          </div>
          <div>${user.email}</divs>
          </div>
          <img src="${user.avatar}">
-         </div>`;
-        
-         card.innerHTML += html
+         </div>`
 
-     let highlightSelect = document.querySelector(`#user${userCount}>.digit`)
-     highlightSelect.className = 'selected' 
-    }   
+        card.innerHTML += html
+
+        let highlightSelect = document.querySelector(`#user${userCount}>.digit`)
+        highlightSelect.className = 'selected'
+    }
 }
 
-function remove (userCount) {
+function remove(userCount) {
     let reset = document.querySelector(`#user${userCount}`)
-    
-    let html = `<div class="digit">${userCount}</div>`;
-        
-         reset.innerHTML = html
 
-     let highlightSelect = document.querySelector(`#user${userCount}>.digit`)
-     highlightSelect.className = 'digit' 
-    }   
+    let html = `<div class="digit">${userCount}</div>`
 
+    reset.innerHTML = html
+
+    let highlightSelect = document.querySelector(`#user${userCount}>.digit`)
+    highlightSelect.className = 'digit'
+}
 
 
 Promise.all([user1, user2, user3])
@@ -47,46 +46,46 @@ Promise.all([user1, user2, user3])
     .then(result => {
         document.addEventListener('click', (ev) => {
             let selectedNum = event.target
-            
-            if (selectedNum.matches('h1')){
-                window.location.hash = `ShowAll`    
+
+            if (selectedNum.matches('h1')) {
+                window.location.hash = `ShowAll`
 
             }
 
-            if (selectedNum.matches('.digit')){
-               
+            if (selectedNum.matches('.digit')) {
+
                 let seletectedDig = Number(selectedNum.innerHTML)
                 let selectedUser = result[seletectedDig - 1]
-                window.location.hash = `#pUser${seletectedDig}`    
+                window.location.hash = `#pUser${seletectedDig}`
             }
-            
+
         })
     })
     .then(result => {
-       
-        window.addEventListener('hashchange',() => {
-         
+
+        window.addEventListener('hashchange', () => {
+
             let urlHash = window.location.hash
             let userHash = urlHash.slice(1, urlHash.length)
             let divHash = urlHash.slice(-1)
             const useArr = [pUser1, pUser2, pUser3]
-            
+
             let userCount = 0
-            if (userHash === 'ShowAll'){
-               useArr.forEach(user => {
+            if (userHash === 'ShowAll') {
+                useArr.forEach(user => {
                     userCount++
-                    render(user, userCount)}
-            )}else{
+                    render(user, userCount)
+                })
+            } else {
                 let removeCount = 0
                 useArr.forEach(user => {
                     removeCount++
-                    remove(removeCount)})
+                    remove(removeCount)
+                })
 
                 render(this[userHash], divHash)
             }
 
 
-    
         })
     })
-    
